165124776e6259cf9317a49fd635b5794d9ffbd6
petkoche <petkoche90@gmail.com>
2017-09-30 17:11:15 +0300

    initial scaffold

---
+++ TelerikAcademy.TripyMate/TelerikAcademy.TripyMate.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TelerikAcademy.TripyMate.Web.MvcApplication" Language="C#" %>
---
+++ TelerikAcademy.TripyMate/TelerikAcademy.TripyMate.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TelerikAcademy.TripyMate.Web.Startup))]
+namespace TelerikAcademy.TripyMate.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
